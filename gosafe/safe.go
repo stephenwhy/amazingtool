499545346cc3a72a51564d85903825bd9c7adfae
StephenWang <v_wanghaiyang2@tal.com>
2024-10-30 22:15:57 +0800

    featg: add go safe func

---
+++ gosafe/safe.go
@@ -0,0 +1,15 @@
+package gosafe
+
+import "log"
+
+// GoSafe runs the given function in a goroutine with panic recovery
+func GoSafe(fn func()) {
+	go func() {
+		defer func() {
+			if r := recover(); r != nil {
+				log.Printf("[Error] Goroutine panic recovered: %v", r)
+			}
+		}()
+		fn()
+	}()
+}
